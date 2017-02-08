b6583f666c52ca9f211c0ee9d620eb2f846c1070
Aschur <suyundukovyf1@yandex.ru>
2017-02-08 21:35:41 +0300

    add AschurStore

---
+++ src/gui/ProductsConsumerTable.java
@@ -0,0 +1,65 @@
+package gui;
+
+import person.ConsumersQueueTeller;
+import person.Person;
+import product.ProductSalesDataTable;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Created by Aschur on 09.12.2016.
+ */
+public class ProductsConsumerTable extends AbstractTableModel {
+
+    private TellerWorkingPlace tellerWorkingPlace;
+
+    public ProductsConsumerTable(TellerWorkingPlace tellerWorkingPlace) {
+        this.tellerWorkingPlace = tellerWorkingPlace;
+    }
+
+    @Override
+    public int getRowCount() {
+
+        ProductSalesDataTable psdt = tellerWorkingPlace.getProductsConsumer();
+        return psdt.size();
+
+    }
+
+    @Override
+    public int getColumnCount() {
+
+        return 2;
+
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+
+
+        ProductSalesDataTable psdt = tellerWorkingPlace.getProductsConsumer();
+
+        switch (columnIndex) {
+            case 0:
+                return psdt.get(rowIndex).getProduct().getName();
+            case 1:
+                return psdt.get(rowIndex).getCount();
+            default:
+                return "";
+        }
+
+    }
+
+    @Override
+    public String getColumnName(int column) {
+
+        String columnName = "";
+        if (column == 0) {
+            columnName = "Product";
+        }else if(column == 1){
+            columnName = "Count";
+        };
+
+        return columnName;
+
+    }
+}
---
+++ src/person/Person.java
@@ -0,0 +1,30 @@
+package person;
+
+/**
+ * Created by Aschur on 03.11.2016.
+ */
+public abstract class Person implements Runnable{
+
+    private String name = "";
+    private static int id = 0;
+
+    public Person() {
+
+        id = ++id;
+        name = getClass().getSimpleName() + "_" + id;
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static int getId() {
+        return id;
+    }
+}
